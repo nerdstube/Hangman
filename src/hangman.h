9405352b17b4e96f076ad143b7497c354dd9e70b
Marc Schulte <bomba@nerdstube.de>
2012-01-03 18:40:38 +0100

    add

---
+++ src/hangman.h
@@ -0,0 +1,16 @@
+#ifndef HANGMAN_H_INCLUDED
+#define HANGMAN_H_INCLUDED
+#include <iostream>
+
+
+//class HANGMAN_H_INCLUDED
+//{
+    //private:
+            //string word;
+            ////int lenght;
+            //int try;
+//
+    //public:
+            //string work();
+
+#endif // HANGMAN_H_INCLUDED
---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdlib>
+#include "hangman.h"
+
+using namespace std;
+
+void clear();
+void ending();
+
+int main()
+{
+
+    string word;
+    int wordlenght, typ;
+    cout<<"Bitte gib das Wort ein:"<<endl;
+    cin>>word;
+    wordlenght=word.size();
+
+    if(wordlenght>=20)
+    {
+        cout<<"Das Spiel wäre zu schwer! Das Wort hat "<<wordlenght<<" Bustaben"<<endl;
+    }else{
+    clear();
+    if(wordlenght<=5)
+     typ=1;
+     else
+     typ=2;
+    }
+
+    switch(typ)
+    {
+    case 1:
+      cout<<"Dein Wort hat "<<wordlenght<< " Bustaben. Du hast 5 Versuche zum lösen!"<<endl;
+      break;
+    case 2:
+      cout<<"Dein Wort hat "<<wordlenght<< " Bustaben. Du hast 7 Versuche zum lösen!"<<endl;
+      break;
+    }
+
+    ending();
+    return 0;
+}
+
+void clear()
+{
+#ifdef WIN32
+system("cls");
+#else
+system("clear");
+#endif
+}
+
+void ending()
+{
+#ifdef WIN32
+system("stop");
+#else
+cout<<endl<<endl;
+cout<<"HAHA! Ich bin ein Linux ich brauch kein System(\"stop\")";
+#endif
+}
