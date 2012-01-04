a0d6ce4237670073ba1e0bfa17580958b7c3881d
Marc Schulte <bomba@nerdstube.de>
2012-01-04 17:33:22 +0100

    add

---
+++ src/os.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include "os.h"
+
+void clear()
+{
+#ifdef WIN32
+system("cls");
+#else
+system("clear");
+#endif
+}
+
+void ending()
+{
+#ifdef WIN32
+system("stop");
+#else
+#endif
+}
